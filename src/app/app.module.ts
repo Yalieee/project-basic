a95a67e617312c677fb912ff5528164f25145388
Yalieee <zeldago2000@gmail.com>
2017-12-03 15:05:59 +0800

    Add Header component and re-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShopppingListComponent } from './shoppping-list/shoppping-list.component';
-import { ShoppingListEditComponent } from './shoppping-list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { ShopppingListComponent } from './header/shoppping-list/shoppping-list.component';
+import { ShoppingListEditComponent } from './header/shoppping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShopppingListComponent,
-    ShoppingListEditComponent
+    ShoppingListEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
